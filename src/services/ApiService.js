ed73f60167b5d3aba3074b5ca15603e56844f149
Paloma <paloma.ditex@gmail.com>
2024-11-08 18:17:33 -0300

    cambio url api

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: '/api/proyectos.json',
+  baseURL: './api/proyectos.json',
 })
 export const fetchAllProyectos = async () => {
   try {

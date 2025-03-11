86ed12c02851c2efc42a8c649edf8b1bf2edcb57
Paloma <paloma.ditex@gmail.com>
2025-03-11 13:30:48 -0300

    update

--- src/components/graphicDesign/DetailBodyGraphicCard.vue
+++ src/components/graphicDesign/DetailBodyGraphicCard.vue
@@ -10,47 +10,47 @@ defineProps({
   <div class="card border-0 rounded-0 bg-transparent p-0 text-center">
     <div class="card-body p-0">
       <div class="row">
-        <div class="col-12">
+        <div class="col-12 mb-2">
           <img
             v-if="proyecto.imagen2 && proyecto.imagen2.length"
             :src="proyecto.imagen2"
-            class="card-img-top rounded-0 h-100 object-fit-cover shadow"
+            class="card-img-top rounded-0 h-100 object-fit-cover shadow-sm"
             :alt="proyecto.nombre"
           />
         </div>
-        <div class="col-12">
+        <div class="col-12 mb-2">
           <img
             v-if="proyecto.imagen3 && proyecto.imagen3.length"
             :src="proyecto.imagen3"
-            class="card-img-top rounded-0 h-100 object-fit-cover shadow"
+            class="card-img-top rounded-0 h-100 object-fit-cover shadow-sm"
             :alt="proyecto.nombre"
           />
         </div>
-        <div class="col-12">
+        <div class="col-12 mb-2">
           <img
             v-if="proyecto.imagen4 && proyecto.imagen4.length"
             :src="proyecto.imagen4"
-            class="card-img-top rounded-0 h-100 object-fit-cover shadow"
+            class="card-img-top rounded-0 h-100 object-fit-cover shadow-sm"
             :alt="proyecto.nombre"
           />
         </div>
-        <div class="col-12">
+        <div class="col-12 mb-2">
           <img
             v-if="proyecto.imagen5 && proyecto.imagen5.length"
             :src="proyecto.imagen5"
-            class="card-img-top rounded-0 h-100 object-fit-cover shadow"
+            class="card-img-top rounded-0 h-100 object-fit-cover shadow-sm"
             :alt="proyecto.nombre"
           />
         </div>
-        <div class="col-12">
+        <div class="col-12 mb-2">
           <img
             v-if="proyecto.imagen6 && proyecto.imagen6.length"
             :src="proyecto.imagen6"
-            class="card-img-top rounded-0 h-100 object-fit-cover shadow"
+            class="card-img-top rounded-0 h-100 object-fit-cover shadow-sm"
             :alt="proyecto.nombre"
           />
         </div>
-        <div class="col-12">
+        <div class="col-12 mb-2">
           <img
             v-if="proyecto.imagen7 && proyecto.imagen7.length"
             :src="proyecto.imagen7"
@@ -58,7 +58,7 @@ defineProps({
             :alt="proyecto.nombre"
           />
         </div>
-        <div class="col-12">
+        <div class="col-12 mb-2">
           <img
             v-if="proyecto.imagen8 && proyecto.imagen8.length"
             :src="proyecto.imagen8"
